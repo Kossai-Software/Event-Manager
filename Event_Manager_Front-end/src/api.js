ab8b4244075dbe48a3cffc4aedab72dc983c2c2a
Kossai-Software <kossaisy2005@gmail.com>
2026-01-31 19:59:36 +0000

    Update api.js

--- Event_Manager_Front-end/src/api.js
+++ Event_Manager_Front-end/src/api.js
@@ -1,11 +1,20 @@
-export async function getEvents() {
-  return [];
-}
+import axios from 'axios';
 
-export async function getCart() {
-  return [];
-}
+const API = axios.create({
+  baseURL: 'http://localhost:5000/api', // Matches server.js port
+});
 
-export async function getNotifications() {
-  return [];
-}
+// backend controllers expect a User ID. We'll default to 1 for now.
+API.interceptors.request.use((config) => {
+  config.headers['x-user-id'] = 1; 
+  return config;
+});
+
+export const getEvents = (params) => API.get('/events', { params });
+export const getCart = () => API.get('/cart');
+export const addToCart = (eventId) => API.post('/cart', { eventId });
+export const createOrder = (orderData) => API.post('/orders', orderData);
+export const getNotifications = () => API.get('/notifications');
+export const createEvent = (formData) => API.post('/events', formData, {
+  headers: { 'Content-Type': 'multipart/form-data' }
+});
