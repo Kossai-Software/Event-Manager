2895c077314708e9efdb510c7e42560fc5324507
Kossai-Software <kossaisy2005@gmail.com>
2026-01-31 19:53:13 +0000

    Update App.jsx

--- Event_Manager_Front-end/src/App.jsx
+++ Event_Manager_Front-end/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
-//import { getEvents, getCart, getNotifications } from './api';
+import { getEvents, getCart, getNotifications } from './api';
 import {
   Search,
   Calendar,
